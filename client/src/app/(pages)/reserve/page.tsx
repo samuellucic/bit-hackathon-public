cd5e73b5ff1c7f4412e482d114a5c74731319e44
Adrian Babić <Adrian.Babic@infobip.com>
2024-10-19 00:08:16 +0200

    date and datetime fields

--- client/src/app/(pages)/reserve/page.tsx
+++ client/src/app/(pages)/reserve/page.tsx
@@ -1,152 +1,257 @@
-"use client";
+'use client';
 
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { TextField, Button, Box, Container, Typography, Grid } from '@mui/material';
+import { DatePicker, DateTimePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import SendIcon from '@mui/icons-material/Send';
+import dayjs from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import 'dayjs/locale/en-gb';
+dayjs.locale('en-gb');
 
-const mandatoryFieldMessage = "Polje je obavezno";
+const mandatoryFieldMessage = 'Polje je obavezno';
 
-const schema = z.object({
+const schema = z
+  .object({
     firstName: z.string().min(1, mandatoryFieldMessage),
     lastName: z.string().min(1, mandatoryFieldMessage),
     address: z.string().min(1, mandatoryFieldMessage),
     city: z.string().min(1, mandatoryFieldMessage),
     oib: z.string().min(1, mandatoryFieldMessage),
-    mobilePhone: z
-        .string()
-        .regex(/^\d+$/, 'Broj mobitela mora sadržavati samo brojeve'),
+    mobilePhone: z.string().regex(/^\d+$/, 'Broj mobitela mora sadržavati samo brojeve'),
     bank: z.string().min(1, mandatoryFieldMessage),
     iban: z
-        .string()
-        .min(15, 'IBAN mora sadržavati najmanje 15 znakova')
-        .max(34, 'IBAN može sadržavati najviše 34 znakova'),
+      .string()
+      .min(15, 'IBAN mora sadržavati najmanje 15 znakova')
+      .max(34, 'IBAN može sadržavati najviše 34 znakova'),
     purpose: z.string().min(1, mandatoryFieldMessage),
-});
-
+    dateOfIssue: z.date().refine((date) => !isNaN(date.getTime()), mandatoryFieldMessage),
+    timeFrom: z.date().refine((date) => !isNaN(date.getTime()), mandatoryFieldMessage),
+    timeTo: z.date().refine((date) => !isNaN(date.getTime()), mandatoryFieldMessage),
+  })
+  .refine((data) => (data.timeFrom && data.timeTo ? data.timeTo > data.timeFrom : true), {
+    message: 'Početak korištenja mora biti prije kraja korištenja',
+    path: ['timeTo'],
+  })
+  .refine(
+    (data) => {
+      if (data.dateOfIssue && data.timeTo) {
+        const dateOfIssue = dayjs(data.dateOfIssue);
+        const timeTo = dayjs(data.timeTo);
+        return timeTo.diff(dateOfIssue, 'day') >= 8;
+      }
+      return true;
+    },
+    {
+      message: 'Datum izdavanja mora biti najmanje 8 dana prije datuma početka korištenja',
+      path: ['dateOfIssue'],
+    }
+  );
 type FormData = z.infer<typeof schema>;
 
-const FormPage: React.FC = () => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<FormData>({
-        resolver: zodResolver(schema),
-    });
+const Page = () => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      dateOfIssue: dayjs().toDate(),
+      timeFrom: dayjs().add(1, 'hour').startOf('hour').add(8, 'day').toDate(),
+      timeTo: dayjs().add(1, 'hour').startOf('hour').add(9, 'day').toDate(),
+    },
+  });
 
-    const onSubmit = (data: FormData) => {
-        console.log(data);
-    };
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
 
-    return (
-        <Container maxWidth="md">
-            <Box sx={{ mt: 4 }}>
-                <Typography variant="h4" gutterBottom style={{marginBottom: "40px"}}>
-                    Zahtjev za rezervaciju doma
-                </Typography>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                fullWidth
-                                label="Ime"
-                                {...register('firstName')}
-                                error={!!errors.firstName}
-                                helperText={errors.firstName?.message}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                fullWidth
-                                label="Prezime"
-                                {...register('lastName')}
-                                error={!!errors.lastName}
-                                helperText={errors.lastName?.message}
-                            />
-                        </Grid>
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Container maxWidth="md">
+        <Box sx={{ mt: 4 }}>
+          <Typography variant="h4" gutterBottom style={{ marginBottom: '40px' }}>
+            Zahtjev za rezervaciju doma
+          </Typography>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  label="Ime"
+                  {...register('firstName')}
+                  error={!!errors.firstName}
+                  helperText={errors.firstName?.message}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  label="Prezime"
+                  {...register('lastName')}
+                  error={!!errors.lastName}
+                  helperText={errors.lastName?.message}
+                />
+              </Grid>
 
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                fullWidth
-                                label="Adresa"
-                                {...register('address')}
-                                error={!!errors.address}
-                                helperText={errors.address?.message}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                fullWidth
-                                label="Grad"
-                                {...register('city')}
-                                error={!!errors.city}
-                                helperText={errors.city?.message}
-                            />
-                        </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  label="Adresa"
+                  {...register('address')}
+                  error={!!errors.address}
+                  helperText={errors.address?.message}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  label="Grad"
+                  {...register('city')}
+                  error={!!errors.city}
+                  helperText={errors.city?.message}
+                />
+              </Grid>
 
-                        <Grid item xs={12}>
-                            <TextField
-                                fullWidth
-                                label="OIB"
-                                {...register('oib')}
-                                error={!!errors.oib}
-                                helperText={errors.oib?.message}
-                            />
-                        </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  label="OIB"
+                  {...register('oib')}
+                  error={!!errors.oib}
+                  helperText={errors.oib?.message}
+                />
+              </Grid>
 
-                        <Grid item xs={12}>
-                            <TextField
-                                fullWidth
-                                label="Broj mobitela"
-                                {...register('mobilePhone')}
-                                error={!!errors.mobilePhone}
-                                helperText={errors.mobilePhone?.message}
-                            />
-                        </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  label="Broj mobitela"
+                  {...register('mobilePhone')}
+                  error={!!errors.mobilePhone}
+                  helperText={errors.mobilePhone?.message}
+                />
+              </Grid>
 
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                fullWidth
-                                label="Banka"
-                                {...register('bank')}
-                                error={!!errors.bank}
-                                helperText={errors.bank?.message}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                fullWidth
-                                label="IBAN"
-                                {...register('iban')}
-                                error={!!errors.iban}
-                                helperText={errors.iban?.message}
-                            />
-                        </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  label="Banka"
+                  {...register('bank')}
+                  error={!!errors.bank}
+                  helperText={errors.bank?.message}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  label="IBAN"
+                  {...register('iban')}
+                  error={!!errors.iban}
+                  helperText={errors.iban?.message}
+                />
+              </Grid>
 
-                        <Grid item xs={12}>
-                            <TextField
-                                fullWidth
-                                label="Opišite svrhu korištenja doma"
-                                multiline
-                                rows={4}
-                                {...register('purpose')}
-                                error={!!errors.purpose}
-                                helperText={errors.purpose?.message}
-                            />
-                        </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label="Kratko opišite svrhu korištenja doma"
+                  multiline
+                  rows={4}
+                  {...register('purpose')}
+                  error={!!errors.purpose}
+                  helperText={errors.purpose?.message}
+                />
+              </Grid>
 
-                        <Grid item xs={12}>
-                            <Button type="submit" variant="contained" color="primary" fullWidth>
-                                Predaj zahtjev
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </form>
-            </Box>
-        </Container>
-    );
+              <Grid item xs={12} sm={6}>
+                <Controller
+                  name="timeFrom"
+                  control={control}
+                  render={({ field }) => (
+                    <DateTimePicker
+                      label="Početak korištenja"
+                      value={field.value ? dayjs(field.value) : null}
+                      onChange={(newValue) => {
+                        field.onChange(newValue ? newValue.toDate() : null);
+                      }}
+                      format="DD/MM/YYYY HH:mm"
+                      slotProps={{
+                        textField: {
+                          fullWidth: true,
+                          error: !!errors.timeFrom,
+                          helperText: errors.timeFrom?.message,
+                        },
+                      }}
+                    />
+                  )}
+                />
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <Controller
+                  name="timeTo"
+                  control={control}
+                  render={({ field }) => (
+                    <DateTimePicker
+                      label="Kraj korištenja"
+                      value={field.value ? dayjs(field.value) : null}
+                      onChange={(newValue) => {
+                        field.onChange(newValue ? newValue.toDate() : null);
+                      }}
+                      format="DD/MM/YYYY HH:mm"
+                      slotProps={{
+                        textField: {
+                          fullWidth: true,
+                          error: !!errors.timeTo,
+                          helperText: errors.timeTo?.message,
+                        },
+                      }}
+                    />
+                  )}
+                />
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <Controller
+                  name="dateOfIssue"
+                  control={control}
+                  render={({ field }) => (
+                    <DatePicker
+                      label="Datum izdavanja"
+                      value={field.value ? dayjs(field.value) : null}
+                      onChange={(newValue) => {
+                        field.onChange(newValue ? newValue.toDate() : null);
+                      }}
+                      format="DD/MM/YYYY"
+                      slotProps={{
+                        textField: {
+                          fullWidth: true,
+                          error: !!errors.dateOfIssue,
+                          helperText: errors.dateOfIssue?.message,
+                        },
+                      }}
+                    />
+                  )}
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <Button type="submit" variant="contained" color="primary" endIcon={<SendIcon />}>
+                  Predaj zahtjev
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </Box>
+      </Container>
+    </LocalizationProvider>
+  );
 };
 
-export default FormPage;
+export default Page;
