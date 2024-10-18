39dbd94f1dc9e581d7b0809adaf243f1bf69afab
Samuel Lucić <samuelsamilucic@gmail.com>
2024-10-18 18:47:08 +0200

    BE: logging aspect

---
+++ server/src/main/java/hr/bithackathon/rental/config/LoggingAspectConfiguration.java
@@ -0,0 +1,14 @@
+package hr.bithackathon.rental.config;
+
+import hr.bithackathon.rental.aop.logging.LoggingAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoggingAspectConfiguration {
+
+    @Bean
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
+    }
+}
